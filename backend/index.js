e02c5ffd4f266731cb84063dcfde540442175728
amitsingh2003 <amitsingh2003plwl@gmail.com>
2025-03-29 02:12:38 +0530

    deployment

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,12 @@
-require('dotenv').config()
-const app = require('./src/app')
+require('dotenv').config();
+const app = require('./src/app');
 
+const cors = require('cors');
 
+app.use(cors({
+    origin: ['http://localhost:5173']
+}));
 
 app.listen(3000, () => {
-    console.log('Server is running on http://localhost:3000')
-})
+    console.log('Server is running on http://localhost:3000');
+});
--- backend/src/app.js
+++ backend/src/app.js
@@ -1,11 +1,7 @@
 const express = require('express');
 const aiRoutes = require('./routes/ai.routes')
-const cors = require('cors')
 
 const app = express()
-
-app.use(cors())
-
 
 app.use(express.json())
 
